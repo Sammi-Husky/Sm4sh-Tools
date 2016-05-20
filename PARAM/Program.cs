d9597397cf7680004155b80f2ea6321c60a1a0de
deihnyx <deinonychus71@hotmail.com>
2016-05-20 04:00:05 +0200

    Adding the args (commited too fast ><)

--- PARAM/Program.cs
+++ PARAM/Program.cs
@@ -12,7 +12,7 @@ namespace Parameters
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);

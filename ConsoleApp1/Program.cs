94e344d9775a4a6de78278cbffa8b5830b399e8e
Sammi-Husky <sammi-husky@live.com>
2017-05-18 17:37:40 -0700

    FITX: Update event dictionary with RE'd names from NovaKirby

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,15 +12,15 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             readInfo(args[0]);
-            using (StreamWriter writer = new StreamWriter("out_desc.cs"))
+            using (StreamWriter writer = new StreamWriter("out_names.cs"))
             {
                 writer.Write("public static Dictionary<uint, string> CMD_NAMES = new Dictionary<uint, string>()\n{ ");
                 for (int i = 0; i < ACMD_INFO.CMD_NAMES.Count; i++)
                 {
                     var id = ACMD_INFO.CMD_NAMES.Keys.ElementAt(i);
-                    var format = $"\"{ACMD_INFO.CMD_DESC[id]}\"";
+                    var format = $"\"{ACMD_INFO.CMD_NAMES[id]}\"";
                     if (string.IsNullOrEmpty(format.Trim('"')))
-                        format = "\"NONE\"";
+                        format = "String.Empty";
                     writer.WriteLine($"{{0x{id:X8},{format}}},");
                 }
                 writer.Write("};");
@@ -33,14 +33,15 @@ namespace ConsoleApp1
                 List<string> raw = stream.ReadToEnd().Split('\n').Select(x => x.Trim('\r')).ToList();
                 raw.RemoveAll(x => String.IsNullOrEmpty(x) || String.IsNullOrWhiteSpace(x) || x.Contains("//"));
 
-                for (int i = 0; i < raw.Count; i += 5)
+                foreach(string line in raw)
                 {
-                    var crc = uint.Parse(raw[i], System.Globalization.NumberStyles.HexNumber);
-                    var Name = raw[i + 1];
-
-                    string[] paramList = raw[i + 2].Split(',').Where(x => x != "NONE").ToArray();
-                    string[] paramSyntax = raw[i + 3].Split(',').Where(x => x != "NONE").ToArray();
-                    ACMD_INFO.SetCMDInfo(crc, paramList.Length + 1, Name, paramList.Select(x => int.Parse(x)).ToArray(), paramSyntax);
+                    var args = line.Split(':');
+                    args[0] = args[0].Trim();
+                    args[1] = args[1].Trim();
+                    if (args[1] == "(UNKNOWN)")
+                        continue;
+                    var id = uint.Parse(args[0].Substring(2), System.Globalization.NumberStyles.HexNumber);
+                    ACMD_INFO.CMD_NAMES[id] = args[1];
                 }
             }
         }
